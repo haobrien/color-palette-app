0775b2856b6c7b7445bc10fbeb4dcbe1eba997a7
Heather OBrien <haobrientx@gmail.com>
2021-08-30 18:20:25 -0500

    Adjust text color based on luminance

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import chroma from 'chroma-js'
 import { Link } from 'react-router-dom'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 import './ColorBox.css'
@@ -21,23 +22,25 @@ export default class ColorBox extends Component {
     render() {
         const { background, name, id, paletteId, showMore } = this.props
         const { copied } = this.state
+        const isDarkColor = (chroma(background).luminance() <= 0.2)
+        const isLightColor = (chroma(background).luminance() >= 0.7)
         return (
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
                 <div style={{ background: background }} className="ColorBox">
                     <div className={`copy-overlay ${copied && 'show'}`} style={{ background: background }}></div>
                     <div className={`copy-message ${copied && 'show'}`}>
                         <h1>Copied!</h1>
-                        <p>{background}</p>
+                        <p className={isLightColor && 'dark-text'}>{background}</p>
                     </div>
                     <div className="copy-container">
                         <div className="box-content">
-                            <span>{name}</span>
+                            <span className={isDarkColor && 'light-text'}>{name}</span>
                         </div>
-                        <button className="copy-button">Copy</button>
+                        <button className={'copy-button ' + (isLightColor && 'dark-text')}>Copy</button>
                     </div>
                     {showMore && (
                         <Link to={`/palette/${paletteId}/${id}`} onClick={evt => evt.stopPropagation()}>
-                            <span className="see-more">More</span>
+                            <span className={'see-more ' + (isLightColor && 'dark-text')}>More</span>
                         </Link>
                     )}
                 </div>
