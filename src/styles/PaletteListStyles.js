35cdf16d6d02d7afc252214d2f6256e9c5288b05
Heather OBrien <haobrientx@gmail.com>
2021-09-05 18:01:28 -0500

    Make paletteList responsive and style

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,13 +1,18 @@
+import bg from './confetti-doodles.svg'
+/* background by SVGBackgrounds.com */
+
 const styles = {
     root: {
-        backgroundColor: '#394bad',
+        backgroundColor: '#e8e8e8',
         height: '100vh',
         display: 'flex',
         alignItems: 'flex-start',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        backgroundImage: `url(${bg})`,
+        overflow: 'scroll'
     },
     container: {
-        width: '50%',
+        width: '60%',
         display: 'flex',
         alignItems: 'flex-start',
         flexDirection: 'column',
@@ -16,12 +21,16 @@ const styles = {
     nav: {
         width: '100%',
         display: 'flex',
-        justifyContent: 'space-between',
         color: 'white',
         alignItems: 'center',
         '& a': {
             textDecoration: 'none',
-            color: 'white'
+            color: 'white',
+            fontWeight:'bold',
+            marginLeft: '1.2rem',
+            '& svg' : {
+                marginRight: '0.5rem'
+            }
         }
     },
     palettes: {
@@ -29,8 +38,22 @@ const styles = {
         boxSizing: 'border-box',
         display: 'grid',
         gridTemplateColumns: 'repeat(3, 30%)',
-        gridGap: '5%',
+        gridGap: '2rem',
+    },
+    [`@media (max-width: 992px)`]: {
+        container: {
+            width: '80%'
+        },
+         palettes: {
+            gridTemplateColumns: 'repeat(2, 49%)',
+        }
+   },
+    [`@media (max-width: 576px)`]: {
+        palettes:{
+            gridTemplateColumns: 'repeat(1, 98%)',
+       }
     }
+
 }
 
 export default styles
